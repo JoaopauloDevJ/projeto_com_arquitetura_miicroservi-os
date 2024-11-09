a640145991fd51f2b93b60f29d3e7f95ea86ceb1
joaopaulo <joaopaulo@hotmail.com>
2024-11-09 14:05:57 -0300

    Projeto vendas online com arquitetura microserviços

---
+++ Product/src/main/java/com/paulo/Product/ProductApplication.java
@@ -0,0 +1,13 @@
+package com.paulo.Product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+}
